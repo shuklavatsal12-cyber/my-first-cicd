88f3f6805ad894aae427b3a38c76f84fbd190156
shuklavatsal12-cyber <shukla.vatsal12@gmail.com>
2026-04-11 13:19:15 +0530

    updated code

--- hello.py
+++ hello.py
@@ -1,13 +1,10 @@
-
 def greet(name):
-    
     return f"Hello, {name}!"
 
-def farewell(name):
 
+def farewell(name):
     return f"Goodbye, {name}!"
 
 
 print(greet("World"))
-
 print(farewell("World"))
